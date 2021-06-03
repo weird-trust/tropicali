da490c0441709fc142902b4ec1e4427a27ade452
weird-trust <robert.burtzik@fork.de>
2021-06-03 18:28:23 +0200

    optimized version of the landingpage

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,57 @@
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var cleanCss = require('gulp-clean-css')
+var sourcemaps = require('gulp-sourcemaps')
 
-gulp.task('default', function() {
+var browserSync = require('browser-sync').create()
+
+var imagemin = require('gulp-imagemin')
+
+sass.compiler = require('node-sass')
+
+gulp.task("sass", function(){
   // we want to run "sass css/app.scss app.css --watch"
-  return gulp.src("css/app.scss")
-    .pipe(sass().on('error', sass.logError))
-});
+  return gulp.src("src/css/app.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(
+      cleanCss({
+        compatibility: 'ie8'
+      })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist"))
+    .pipe(browserSync.stream())
+})
+
+gulp.task("html", function(){
+  return gulp.src("src/*.html")
+    .pipe(gulp.dest("dist"))
+})
+
+gulp.task("fonts", function(){
+  return gulp.src("src/fonts/*")
+    .pipe(gulp.dest("dist/fonts"))
+})
+
+gulp.task("images", function(){
+  return gulp.src("src/img/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/img"))
+})
+
+gulp.task("watch", function(){
+
+  browserSync.init({
+    server: {
+      baseDir:"dist"
+    }
+  })
+  gulp.watch("src/*.html", ["html"]).on("change", browserSync.reload)
+  gulp.watch("src/css/app.scss", ["sass"])
+  gulp.watch("src/fonts/*", ["fonts"])
+  gulp.watch("src/img/*", ["images"])
+})
+
+gulp.task('default',["html","sass", "fonts", "images", "watch"]);
