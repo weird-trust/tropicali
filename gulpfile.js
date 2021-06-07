055b6d2f8d4c74490309e5cb129d488d741941dd
weird-trust <robert.burtzik@fork.de>
2021-06-07 12:20:55 +0200

    site ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 
 var gulp = require('gulp')
-var sass = require('gulp-sass')
+
 var cleanCss = require('gulp-clean-css')
+var postcss = require('gulp-postcss')
 var sourcemaps = require('gulp-sourcemaps')
+var concat = require('gulp-concat')
 
 var browserSync = require('browser-sync').create()
 
@@ -10,13 +12,25 @@ var imagemin = require('gulp-imagemin')
 
 var ghpages = require('gh-pages')
 
-sass.compiler = require('node-sass')
 
-gulp.task("sass", function(){
-  // we want to run "sass css/app.scss app.css --watch"
-  return gulp.src("src/css/app.scss")
+
+gulp.task("css", function(){
+    return gulp.src([
+      "src/css/reset.css",
+      "src/css/typography.css",
+      "src/css/app.css"
+    ])
     .pipe(sourcemaps.init())
-    .pipe(sass())
+    .pipe(
+      postcss([
+        require("autoprefixer"),
+        require("postcss-preset-env")({ 
+          stage: 1,
+          browsers: ["IE 11", "last 2 versions"]
+        })
+      ])
+      )
+    .pipe(concat("app.css"))  
     .pipe(
       cleanCss({
         compatibility: 'ie8'
@@ -51,7 +65,7 @@ gulp.task("watch", function () {
     }
   })
   gulp.watch("src/*.html", ["html"]).on("change", browserSync.reload)
-  gulp.watch("src/css/app.scss", ["sass"])
+  gulp.watch("src/css/*.css", ["css"])
   gulp.watch("src/fonts/*", ["fonts"])
   gulp.watch("src/img/*", ["images"])
 })
@@ -60,4 +74,4 @@ gulp.task("deploy", function () {
   ghpages.publish("dist")
 })
 
-gulp.task('default',["html","sass", "fonts", "images", "watch"]);
+gulp.task('default',["html","css", "fonts", "images", "watch"]);
